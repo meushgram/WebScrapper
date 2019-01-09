1d701bc657c45e890879314ef1db240aab947ba8
meushgram <mrdhanushkumar@gmail.com>
2019-01-09 14:49:01 +0530

    Uploaded at THBS

--- downloadAsPDF.js
+++ downloadAsPDF.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 async function run(){
-const browser = await puppeteer.launch({headless: false});
+const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  await page.goto('https://www.google.com/search?num=50&newwindow=1&rlz=1C1CHBF_enIN822IN822&ei=XNA0XLupOozdvgTz_6qoDg&q=async+browser+in+puppeteer&oq=async+browser+in+puppeteer&gs_l=psy-ab.3..33i21.159390.169165..169349...6.0..0.235.5330.0j24j4......0....1..gws-wiz.......0j0i71j35i39j0i67j0i131j0i20i263j0i22i10i30j0i22i30j33i160j33i22i29i30.IA0Rw6EnBlE');
-  await page.pdf({path : 'sourcex.pdf'});
+  await page.goto('https://www.google.com/search?num=50&newwindow=1&safe=strict&rlz=1C1GCEU_enIN822IN823&ei=YXo1XNisBae40PEPpY-ymAk&q=thbs+mis&oq=thbs&gs_l=psy-ab.3.0.0i71l8.0.0..2435...0.0..0.0.0.......0......gws-wiz.p9I7nxDCIHw');
+  await page.screenshot({path : 'save2.png'});
   browser.close();
 }
 run();
